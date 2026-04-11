a44920f1217022cd3c8a272f9b92d66876f64221
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-04-11 13:06:41 +0900

    auto: update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { CustomCursor } from "@/components/ui/CustomCursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
