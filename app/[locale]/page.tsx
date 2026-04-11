94e31e3ff0ee8b65762d84a9f248a73ee1404590
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-04-11 13:04:22 +0900

    auto: update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import { About } from "@/components/sections/About";
 import { Experience } from "@/components/sections/Experience";
 import { Projects } from "@/components/sections/Projects";
 import { TechStack } from "@/components/sections/TechStack";
+import { Contact } from "@/components/sections/Contact";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Experience />
       <Projects />
       <TechStack />
+      <Contact />
     </>
   );
 }
