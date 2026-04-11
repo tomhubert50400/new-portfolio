c3d4249b72aac7dc1b1eedf1387471afa64f7818
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-04-11 21:31:00 +0900

    auto: update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <CustomCursor />
         {children}
       </body>
     </html>
